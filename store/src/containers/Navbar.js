5348d3616f7aa4de4ee464740d6fc7972fac2813
David Loveday <davemech@yahoo.com>
2019-04-22 15:16:31 -0700

    format code:
    no additional functionality added

--- store/src/containers/Navbar.js
+++ store/src/containers/Navbar.js
@@ -1,25 +1,43 @@
-
 import React from "react";
-import {NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import '../css/Navbar.css'
+import "../css/Navbar.css";
 const Navbar = () => {
-    return (
-  <nav className="navbar navbar-expand-lg navbar-light">
-  <NavLink className="navbar-brand" to="/">Assemble Store</NavLink>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon" ></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">      
-    </ul>
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search for products" aria-label="Search" />
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-    )
-}
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <NavLink className="navbar-brand" to="/">
+        Assemble Store
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto" />
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search for products"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- store/src/containers/Order.js
+++ store/src/containers/Order.js
@@ -1,102 +1,150 @@
-import React, { Component } from 'react'
-import {NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
-import "../css/Order.css"
+import "../css/Order.css";
 
-
-class Order extends Component {  
-    
-    render() {
-        return (
-
-            <div className="">
-                <div className="Header">Check Out</div>
-                <div className="container order-container">
-            <div className="row">
-                <div className="col-6">
-            <form>           
-            <div className="form-group">
-              <label for="inputAddress">Name on Card</label>
-              <input type="text" className="form-control" id="inputAddress" placeholder="Name on Card" />
+class Order extends Component {
+  render() {
+    return (
+      <div className="">
+        <div className="Header">Check Out</div>
+        <div className="container order-container">
+          <div className="row">
+            <div className="col-6">
+              <form>
+                <div className="form-group">
+                  <label for="inputAddress">Name on Card</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputAddress"
+                    placeholder="Name on Card"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="inputAddress2">Card Number</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputAddress2"
+                    placeholder="XXXX XXXX XXXX XXXX"
+                  />
+                </div>
+                <div className="form-group">
+                  <form method="post">
+                    <div class="form-group">
+                      <label class="control-label" for="date">
+                        Expiration Date
+                      </label>
+                      <input
+                        class="form-control"
+                        id="date"
+                        name="date"
+                        placeholder="MM/DD/YYY"
+                        type="text"
+                      />
+                    </div>
+                    <div class="form-group" />
+                  </form>
+                </div>
+                <div className="form-group">
+                  <label for="inputAddress2">CVV</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputAddress2"
+                    placeholder="CVV"
+                  />
+                </div>
+              </form>
             </div>
-            <div className="form-group">
-              <label for="inputAddress2">Card Number</label>
-              <input type="text" className="form-control" id="inputAddress2" placeholder="XXXX XXXX XXXX XXXX" />
-            </div>                
-            <div className="form-group">
-            <form method="post">
-            <div class="form-group"> 
-              <label class="control-label" for="date">Expiration Date</label>
-              <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
+            <div className="col-6">
+              <form>
+                <div className="form-row">
+                  <div className="form-group col-md-6">
+                    <label for="inputEmail4">First Name</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="inputEmail4"
+                      placeholder="First Name"
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label for="inputPassword4">Last Name</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="inputPassword4"
+                      placeholder="Last Name"
+                    />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label for="inputAddress">Address</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputAddress"
+                    placeholder="1234 Main St"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="inputAddress2">Address 2</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputAddress2"
+                    placeholder="Apartment, studio, or floor"
+                  />
+                </div>
+                <div className="form-row">
+                  <div className="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="inputCity"
+                    />
+                  </div>
+                  <div className="form-group col-md-4">
+                    <label for="inputState">State</label>
+                    <select id="inputState" className="form-control">
+                      <option selected>Choose...</option>
+                      <option>...</option>
+                    </select>
+                  </div>
+                  <div className="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" className="form-control" id="inputZip" />
+                  </div>
+                </div>
+              </form>
             </div>
-            <div class="form-group">         
-            </div>
-           </form>
-            </div>                
-            <div className="form-group">
-              <label for="inputAddress2">CVV</label>
-              <input type="text" className="form-control" id="inputAddress2" placeholder="CVV" />
-            </div>                
-          </form>
-          </div>
-          <div className="col-6">
-          <form>
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <label for="inputEmail4">First Name</label>
-                <input type="email" className="form-control" id="inputEmail4" placeholder="First Name" />
-              </div>
-              <div className="form-group col-md-6">
-                <label for="inputPassword4">Last Name</label>
-                <input type="password" className="form-control" id="inputPassword4" placeholder="Last Name" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label for="inputAddress">Address</label>
-              <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
-            </div>
-            <div className="form-group">
-              <label for="inputAddress2">Address 2</label>
-              <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-            </div>
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" className="form-control" id="inputCity" />
-              </div>
-              <div className="form-group col-md-4">
-                <label for="inputState">State</label>
-                <select id="inputState" className="form-control">
-                  <option selected>Choose...</option>
-                  <option>...</option>
-                </select>
-              </div>
-              <div className="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" className="form-control" id="inputZip" />
-              </div>
-            </div>
-           
-          </form>
-              </div>
-              <div className="ctn">
+            <div className="ctn">
               <div className="form-group">
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" id="gridCheck" />
-                <label className="form-check-label" for="gridCheck">
-                  Billing address as the same
-                </label>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="gridCheck"
+                  />
+                  <label className="form-check-label" for="gridCheck">
+                    Billing address as the same
+                  </label>
+                </div>
               </div>
+              <NavLink to="/complete">
+                <button type="submit" className="btn btn-primary">
+                  PLACE ORDER
+                </button>
+              </NavLink>
             </div>
-            <NavLink to="/complete">
-            <button type="submit" className="btn btn-primary">PLACE ORDER</button>
-            </NavLink>
-            </div>
-                 </div>
-                 </div>
-                 </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Order;
